744c648a47ef647ad243b77d06240d7b9c036a2a
leemaum <dlakaoo2@gmail.com>
2020-07-07 21:14:35 +0900

    add js

--- colors.js
+++ colors.js
@@ -1,32 +1,36 @@
+var Links = {
+    setColor: function (color) {
+//        var alist = document.querySelectorAll('a');
+//        var i = 0;
+//        while (i < alist.length) {
+//            alist[i].style.color = color;
+//            i++;
+//        }
+        $('a').css('color', color);
+    }
+} 
+
 var Body = {
-            setBackgroundColor:function(color){
-            document.querySelector('body').style.backgroundColor=color;
-             },
-             setColor:function(color){
-            document.querySelector('body').style.color=color;
-             }
-        }
-        var Links = {
-            setColor:function(color){
-                var alist = document.querySelectorAll('a');
-               var i = 0;
-                while (i < alist.length){
-                    alist[i].style.color = color;
-                     i++;
-                 }
-            }
-        }
-        function handler(self){
-        if(self.value === 'night'){
-            Body.setBackgroundColor('black');
-            Body.setColor('white');
-            Links.setColor('skyblue');
-            self.value = 'day';
-        }
-        else{
-            Body.setBackgroundColor('white');
-            Body.setColor('black');            
-            Links.setColor('blue');
-            self.value = 'night';  
-        }
-        }
+    setBackgroundColor: function (color) {
+//        document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+    },
+    setColor: function (color) {
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    }
+}
+
+function handler(self) {
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        Links.setColor('skyblue');
+        self.value = 'day';
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        Links.setColor('blue');
+        self.value = 'night';
+    }
+}
